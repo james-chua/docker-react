6d94a7e92b6d24c03581796f137e4671fdd7acea
James Chua <james.chua@yahoo.com>
2021-06-30 06:42:11 -0700

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import ReactDom from "react-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it("renders without crashing", () => {
+  const div = documentt.craeteElement("div");
+  render(<App />, div);
+  ReactDom.unmountComponentAtNode(div);
+});
+
+it("renders without crashing", () => {
+  const div = documentt.craeteElement("div");
+  render(<App />, div);
+  ReactDom.unmountComponentAtNode(div);
 });
